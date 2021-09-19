08d8f8fc5e9f30d7f89f4e4ab9f570f06e2c7ac8
Shkondita <shkondita1994@gmail.com>
2021-09-19 17:10:08 +0300

    спрашиваю имя с помощью js

---
+++ Week13Lesson/functions.js
@@ -0,0 +1,32 @@
+// alert('Привет!');
+
+// let age = prompt('How old are you?'. 30);
+
+// alert(`You are ${age} years old!'`);
+
+// let yes = confirm('Продолжим программировать?');
+// alert('Вы выбрали ' + yes);
+
+function showMessage () {
+    alert('Привет всем!');
+}
+
+function sum(a,b) {
+    return a + b;
+}
+
+// alert(sum(5,6));
+
+function y(x) {
+    return x ** 2;
+}
+
+let result = y(2);
+// alert(result);
+
+functionshowMessageFromUser(name,message)
+{
+    alert(name+': '+message);
+}
+
+alert(showMessageFromUser(Алиса))
---
+++ Week13Lesson/scripts.js
@@ -0,0 +1,49 @@
+let message = 'Привет!';
+let message2;
+message2 = 'мир!';
+
+let x = 5;
+let y = x;
+x = 3;
+
+let user = 'Алиса', 
+    age = 34,
+    number = '94675467';
+
+// let user = 'Алиса';
+// let age = 34;
+// let number = '94675467';
+
+// Раньше вместо let использовали var
+
+var age2 = 30;
+
+const CONST = 100;
+
+let n = 123;
+n = 12.3;
+
+alert(x * y);
+alert(n * 10);
+
+alert(n / 0);
+// alert(n / user);
+
+let name1 = 'Алиса';
+let name2 = "Алиса";
+let name3 = `Алиса`;
+
+let message_to = `Привет, ${name1}`;
+let message_to2 = 'Привет, ' + name2;
+
+// alert(message_to2);
+
+let yes = true;
+let no = false;
+
+// let a = undefined;
+
+let a;
+alert(a);
+
+// let b = null;
